c84ec87108dbcb6a7a4ab8656c61a11325a82e50
Diallooptional <diallooptional@gmail.com>
2020-04-22 15:38:13 -0400

    Update index.js
    Added Notification functions 2/2

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -49,73 +49,31 @@ exports.receivePositif = functions.firestore.document('positives/{doc}').onCreat
 *****************************************************************/   
 
 
-exports.notifyContacted = functions.region('europe-west1').database.ref(`/users/{user_id}/user_trans_history/{tr_id}`)
-   .onCreate(async (snap, context) => {    
+exports.notifyContacted = functions.firestore
+  .document("users/{uid}").onWrite((change, context) => { 
+  
+  const data = change.after.data();
 
-    const transaction = snap.val();
-    const receiverUid = transaction.tr_receiver_id;
-    const senderPhone = transaction.tr_sender_phone_num;
-    const amountReceived = transaction.tr_amount;
-    const tempReceiverRef = snap.ref.parent;
-    const temp = tempReceiverRef.parent;
-
-    //If it's the sender's node do not send notification
-    if (new String(temp).valueOf() !== new String(admin.database().ref(`/users/${receiverUid}`)).valueOf()) {
-      console.log("No need to send notification to sender. Notification Canceled");
-      console.log("This: " + temp + " is the same..");
-      console.log("..as this: " + admin.database().ref(`/users/${receiverUid}`));
-      return;
-    }
-
-// Get the list of device notification tokens.
-    const getDeviceTokensPromise = admin.database()
-        .ref(`/users/${receiverUid}/user_tokens`).once('value');
-
-    // Get the follower profile.
-    const getSenderProfilePromise = admin.auth().getUser(receiverUid);
-
-    // The snapshot to the user's tokens.
-    let tokensSnapshot;
-
-    // The array containing all the user's tokens.
-    let tokens;
-
-    const results = await Promise.all([getDeviceTokensPromise, getSenderProfilePromise]);
-    tokensSnapshot = results[0];
-    const sender = results[1];
-
-    // Check if there are any device tokens.
-    if (!tokensSnapshot.hasChildren()) {
-      return console.log('There are no notification tokens to send to.');
-    }
-    console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to.');
-
-
-  // Notification details.
+  const uid = data.uid;
+  const case_id = data.notified_by;
+  const token = data.device_token;
+  
+   // Notification details.
   const payload = {
     notification: {
-      title: 'Paiement reçu!',
-      body: `${senderPhone} vous a envoyer `.concat(amountReceived).concat("FG")
+      title: 'Avis de proximité!',
+      body: 'Rendez-vous dans votre profile plus de details'
     }
   };
 
-
-// Listing all tokens as an array.
-  tokens = Object.keys(tokensSnapshot.val());
-  // Send notifications to all tokens.
-  const response = await admin.messaging().sendToDevice(tokens, payload);
-  // For each message check if there was an error.
-  const tokensToRemove = [];
-  response.results.forEach((result, index) => {
-    const error = result.error;
-    if (error) {
-      console.error('Failure sending notification to', tokens[index], error);
-      // Cleanup the tokens who are not registered anymore.
-      if (error.code === 'messaging/invalid-registration-token' ||
-          error.code === 'messaging/registration-token-not-registered') {
-        tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
-      }
-    }
+  // Send notifications to token.
+  admin.messaging().sendToDevice(token, payload)
+   .then((response) => {
+    // Response is a message ID string.
+    console.log('Successfully sent message:', response);
+  })
+  .catch((error) => {
+    console.log('Error sending message:', error);
   });
-  return Promise.all(tokensToRemove);
+
 });
